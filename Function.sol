5df0192f9469f19f66be80446b26166d99abef92
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:01:33 +0100

    Create Function.sol

---
+++ Function.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract FunctionIntro {
+    function sub(uint256 x, uint256 y) external pure returns (uint256) {
+        return x - y;
+    }
+}
