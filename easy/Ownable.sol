0e437c964395e091d738cc6453e850d1f7c14b62
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:43:36 +0100

    Update Ownable.sol

--- easy/Ownable.sol
+++ easy/Ownable.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    function setOwner(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "new owner = zero address");
+        owner = newOwner;
+    }
+}
