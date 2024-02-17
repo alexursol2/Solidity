44a0908e7cf848c61e73e92c8fdb2c63fe003213
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:50:21 +0100

    Update Unchecked_Math.sol

--- easy/Unchecked_Math.sol
+++ easy/Unchecked_Math.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract UncheckedMath {
+    function add(uint256 x, uint256 y) external pure returns (uint256) {
+        // 22291 gas
+        // return x + y;
+
+        // 22103 gas
+        unchecked {
+            return x + y;
+        }
+    }
+
+    function sub(uint256 x, uint256 y) external pure returns (uint256) {
+        unchecked {
+            return x - y;
+        }
+    }
+
+    function sumOfSquares(uint256 x, uint256 y)
+        external
+        pure
+        returns (uint256)
+    {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint256 x2 = x * x;
+            uint256 y2 = y * y;
+
+            return x2 + y2;
+        }
+    }
+
+    function sumOfCubes(uint256 x, uint256 y) external pure returns (uint256) {
+        unchecked {
+            return x * x * x + y * y *y;
+        }
+    }
+}
