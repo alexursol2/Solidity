e3ef9f72a04fcecf138cd2e143998aebdaf4b384
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:24:33 +0100

    Create If_and_Else.sol

---
+++ If_and_Else.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract IfElse {
+    function ifElse(uint256 x) external pure returns (uint256) {
+        if (x < 10) {
+            return 1;
+        } else if (x < 20) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    function ternaryOperator(uint256 x) external pure returns (uint256) {
+        // condition ? value to return if true : value to return if false
+        return x > 1 ? 10 : 20;
+    }
+
+    function exercise_1(uint256 x) external pure returns (uint256) {
+        return x > 0 ? 1 : 0;
+    }
+
+    function exercise_2(uint256 x) external pure returns (uint256) {
+        return x > 0 ? 1 : 0;
+    }
+}
