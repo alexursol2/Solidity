33fb8166c44bf5421afcc8dc14a1375428266caa
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:51:41 +0100

    Update Custom_Error.sol

--- easy/Custom_Error.sol
+++ easy/Custom_Error.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract CustomError {
+    error MyError(address caller, uint256 i);
+
+    error InvalidAddress();
+    error NotAuthorized(address caller);
+
+    address public owner = msg.sender;
+
+    function myError(uint256 i) external {
+        if (i < 10) {
+            revert MyError(msg.sender, i);
+        }
+    }
+
+    function setOwner(address _owner) external {
+        if (_owner == address(0)) {
+            revert InvalidAddress();
+        }
+        if (msg.sender != owner) {
+            revert NotAuthorized(msg.sender);
+        }
+        owner = _owner;
+    }
+}
