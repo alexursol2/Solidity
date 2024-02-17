4febdf0cab913577b5471d5d5e8be82cf3092dcd
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-17 14:47:27 +0100

    Update Debug_with_Foundry.sol

--- easy/Debug_with_Foundry.sol
+++ easy/Debug_with_Foundry.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import {console} from "forge-std/Test.sol";
 
+contract Token {
+    mapping(address => uint256) public balances;
+
+    constructor() {
+        balances[msg.sender] = 100;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        console.log("transfer", msg.sender, to, amount);
+    }
+}
