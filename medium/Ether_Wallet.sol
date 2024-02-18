4b44a07ae053ad0d01b500681bd091b6e4691833
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-18 08:46:16 +0100

    Update Ether_Wallet.sol

--- medium/Ether_Wallet.sol
+++ medium/Ether_Wallet.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+    
+    function withdraw(uint256 amount) external{
+        require(msg.sender == owner, "not owner");
+        (bool sent,) = owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+    
+    receive() external payable{}
+}
