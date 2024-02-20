4d10b3672e76e77e276e5fce59c7afca6eacb9e7
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-20 21:36:30 +0100

    Update Most_Significant_Bit.sol

--- medium/Most_Significant_Bit.sol
+++ medium/Most_Significant_Bit.sol
@@ -7,7 +7,6 @@ contract MostSignificantBit {
         pure
         returns (uint8 r)
     {
-        function findMostSignificantBit(uint256 x) external pure returns (uint8 r) {
         // x >= 2 ** 128
         if (x >= 0x100000000000000000000000000000000) {
             x >>= 128;
@@ -46,6 +45,53 @@ contract MostSignificantBit {
         // x >= 2 ** 1
         if (x >= 0x2) r += 1;
     }
+}
 
+contract MostSignificantBitAssembly {
+    function mostSignificantBit(uint256 x)
+        external
+        pure
+        returns (uint256 msb)
+    {
+        assembly {
+            let f := shl(7, gt(x, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF))
+            x := shr(f, x)
+            // or can be replaced with add
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(6, gt(x, 0xFFFFFFFFFFFFFFFF))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(5, gt(x, 0xFFFFFFFF))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(4, gt(x, 0xFFFF))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(3, gt(x, 0xFF))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(2, gt(x, 0xF))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := shl(1, gt(x, 0x3))
+            x := shr(f, x)
+            msb := or(msb, f)
+        }
+        assembly {
+            let f := gt(x, 0x1)
+            msb := or(msb, f)
+        }
     }
 }
