18e9f9e03664795334c1a04a7178bf4cdb6a53f1
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-22 19:40:47 +0100

    Update Piggy_Bank.sol

--- hard/Piggy_Bank.sol
+++ hard/Piggy_Bank.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract PiggyBank {
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    address public owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    receive() external payable {
+        emit Deposit(msg.value);
+    }
+    
+    function withdraw() external {
+        require(msg.sender == owner, "not owner");
+        emit Withdraw(address(this).balance);
+        selfdestruct(payable(msg.sender));
+    }
+}
