c2925eec916c7972dde3596d4bd2b1ab83ca0245
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-02-26 19:50:44 +0100

    Update Dutch_Auction.sol

--- DeFi/Dutch_Auction.sol
+++ DeFi/Dutch_Auction.sol
@@ -1,1 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import {IERC721} from "sce/sol/IERC721.sol";
+
+contract DutchAuction {
+    uint256 private constant DURATION = 7 days;
+
+    IERC721 public immutable nft;
+    uint256 public immutable nftId;
+
+    address payable public immutable seller;
+    uint256 public immutable startingPrice;
+    uint256 public immutable startAt;
+    uint256 public immutable expiresAt;
+    uint256 public immutable discountRate;
+
+    constructor(
+        uint256 _startingPrice,
+        uint256 _discountRate,
+        address _nft,
+        uint256 _nftId
+    ) {
+        seller = payable(msg.sender);
+        startingPrice = _startingPrice;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+        discountRate = _discountRate;
+
+        require(
+            _startingPrice >= _discountRate * DURATION, "starting price < min"
+        );
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
+
+    function getPrice() public view returns (uint256) {
+        return startingPrice - (discountRate * (block.timestamp - startAt));
+
+    }
+
+    function buy() external payable {
+        require(block.timestamp < expiresAt, "auction expired");
+    
+        uint256 price = getPrice();
+        require(msg.value >= price, "ETH < price");
+    
+        nft.transferFrom(seller, msg.sender, nftId);
+        uint256 refund = msg.value - price;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+        selfdestruct(seller);
+    }
+
+}
