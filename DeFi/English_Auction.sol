4fdcc4846fabddc8cf99e651e2b0f15c1eadd233
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-08 18:35:36 +0100

    Update English_Auction.sol

--- DeFi/English_Auction.sol
+++ DeFi/English_Auction.sol
@@ -1,1 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import {IERC721} from "sce/sol/IERC721.sol";
+
+contract EnglishAuction {
+    event Start();
+    event Bid(address indexed sender, uint256 amount);
+    event Withdraw(address indexed bidder, uint256 amount);
+    event End(address winner, uint256 amount);
+
+    IERC721 public immutable nft;
+    uint256 public immutable nftId;
+
+    address payable public immutable seller;
+    uint256 public endAt;
+    bool public started;
+    bool public ended;
+
+    address public highestBidder;
+    uint256 public highestBid;
+    // mapping from bidder to amount of ETH the bidder can withdraw
+    mapping(address => uint256) public bids;
+
+    constructor(address _nft, uint256 _nftId, uint256 _startingBid) {
+        nft = IERC721(_nft);
+        nftId = _nftId;
+        seller = payable(msg.sender);
+        highestBid = _startingBid;
+    }
+
+    function start() external {
+        require(!started, "started");
+        require(msg.sender == seller, "not seller");
+    
+        nft.transferFrom(msg.sender, address(this), nftId);
+        started = true;
+        endAt = block.timestamp + 7 days;
+    
+        emit Start();
+    }
+
+    function bid() external payable {
+        require(started, "not started");
+        require(block.timestamp < endAt, "ended");
+        require(msg.value > highestBid, "value < highest");
+    
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+    
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+    
+        emit Bid(msg.sender, msg.value);
+    }
+
+
+    function withdraw() external {
+        uint256 bal = bids[msg.sender];
+        bids[msg.sender] = 0;
+        payable(msg.sender).transfer(bal);
+        emit Withdraw(msg.sender, bal);
+    }
+
+
+    function end() external {
+        require(started, "not started");
+        require(block.timestamp >= endAt, "not ended");
+        require(!ended, "ended");
+    
+        ended = true;
+        if (highestBidder != address(0)) {
+            nft.transferFrom(address(this), highestBidder, nftId);
+            seller.transfer(highestBid);
+        } else {
+            nft.transferFrom(address(this), seller, nftId);
+        }
+    
+        emit End(highestBidder, highestBid);
+    }
+
+}
