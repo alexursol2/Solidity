f6b34e766b02df5144dd5385fca672722e46765e
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 12:55:40 +0100

    Update Reentrancy_Guard.sol

--- Security/Reentrancy_Guard.sol
+++ Security/Reentrancy_Guard.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract ReentrancyGuard {
+    // Count stores number of times the function func was called
+    uint256 public count;
+    bool private locked;
+
+    modifier lock() {
+        require(!locked, "locked");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function exec(address target) external lock {
+        (bool ok,) = target.call("");
+        require(ok, "call failed");
+        count += 1;
+    }
+}
