014c9e778c667c61aab763518aaff2d3836d5cc7
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-10 15:19:57 +0100

    Update Flash_Swap.sol

--- UniSwap/Flash_Swap.sol
+++ UniSwap/Flash_Swap.sol
@@ -1,1 +1,84 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "sce/sol/IERC20.sol";
+
+interface IUniswapV2Factory {
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
+}
+
+interface IUniswapV2Callee {
+    function uniswapV2Call(
+        address sender,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external;
+}
+
+interface IUniswapV2Pair {
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+}
+
+contract UniswapV2FlashSwap is IUniswapV2Callee {
+    address private constant UNISWAP_V2_FACTORY =
+        0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+
+    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    IUniswapV2Factory private constant factory =
+        IUniswapV2Factory(UNISWAP_V2_FACTORY);
+
+    IERC20 private constant weth = IERC20(WETH);
+
+    IUniswapV2Pair private immutable pair;
+
+    constructor() {
+        pair = IUniswapV2Pair(factory.getPair(DAI, WETH));
+    }
+
+    function flashSwap(uint256 wethAmount) external {
+        // Need to pass some data to trigger uniswapV2Call
+        bytes memory data = abi.encode(WETH, msg.sender);
+
+        // amount0Out is DAI, amount1Out is WETH
+        pair.swap(0, wethAmount, address(this), data);
+    }
+
+    // This function is called by the DAI/WETH pair contract
+    function uniswapV2Call(
+        address sender,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external {
+        require(msg.sender == address(pair), "not pair");
+        require(sender == address(this), "not sender");
+
+        (address tokenBorrow, address caller) =
+            abi.decode(data, (address, address));
+
+        // Your custom code would go here. For example, code to arbitrage.
+        require(tokenBorrow == WETH, "token borrow != WETH");
+
+        // about 0.3% fee, +1 to round up
+        uint256 fee = ((amount1 * 3) / 997) + 1;
+        uint256 amountToRepay = amount1 + fee;
+
+        // Transfer flash swap fee from caller
+        weth.transferFrom(caller, address(this), fee);
+
+        // Repay
+        weth.transfer(address(pair), amountToRepay);
+    }
+}
