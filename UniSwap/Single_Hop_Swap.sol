ca198362f5b3f3bb322059e674f5e441d7f6bb61
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-03-10 14:38:49 +0100

    Create Single_Hop_Swap.sol

---
+++ UniSwap/Single_Hop_Swap.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "sce/sol/IERC20.sol";
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+contract UniswapV2SingleHopSwap {
+    address private constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+
+    IUniswapV2Router private constant router =
+        IUniswapV2Router(UNISWAP_V2_ROUTER);
+    IERC20 private constant weth = IERC20(WETH);
+    IERC20 private constant dai = IERC20(DAI);
+
+    function swapSingleHopExactAmountIn(uint256 amountIn, uint256 amountOutMin)
+        external
+    {
+        weth.transferFrom(msg.sender, address(this), amountIn);
+        weth.approve(address(router), amountIn);
+
+        address[] memory path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+
+        router.swapExactTokensForTokens(
+            amountIn, amountOutMin, path, msg.sender, block.timestamp
+        );
+    }
+
+    function swapSingleHopExactAmountOut(
+        uint256 amountOutDesired,
+        uint256 amountInMax
+    ) external {
+        weth.transferFrom(msg.sender, address(this), amountInMax);
+        weth.approve(address(router), amountInMax);
+
+        address[] memory path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+
+        uint256[] memory amounts = router.swapTokensForExactTokens(
+            amountOutDesired, amountInMax, path, msg.sender, block.timestamp
+        );
+
+        // Refund WETH to msg.sender
+        if (amounts[0] < amountInMax) {
+            weth.transfer(msg.sender, amountInMax - amounts[0]);
+        }
+    }
+}
